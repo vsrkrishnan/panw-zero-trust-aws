aabd7fe743651fea02f975600d135e705f107aa7
V. Shivaramakrishnan <vsrkrishnan.1987@gmail.com>
2022-05-21 16:22:29 +0530

    AWS Zero Trust Lab with VM-Series and CN-Series

--- terraform/modules/vpc/variables.tf
+++
@@ -1,9 +0,0 @@
-
-variable "vpc" { default = {} }
-variable "subnets" { default = [] }
-variable "route-tables" { default = [] }
-variable "security-groups" { default = [] }
-variable "ec2-instances" { default = [] }
-
-variable "prefix-name-tag" { default = "" }
-variable "global_tags" { default = {} }
--- terraform/modules/vpc_routes/variables.tf
+++
@@ -1,6 +0,0 @@
-
-variable "vpc-routes" { default = [] }
-variable "vpcs" { default = {} }
-variable "tgw-ids" { default = {} }
-variable "prefix-name-tag" { default = "" }
-variable "ngfw-data-eni" { default = "" }
