bb5ad25632b86979ef77ede42170adf6ca8517f8
V. Shivaramakrishnan <vsrkrishnan.1987@gmail.com>
2022-05-23 16:09:03 +0530

    Updating the setup script to run VM or CN series deployments through CLI args.

--- setup.sh
+++ setup.sh
@@ -19,22 +19,38 @@ function install_terraform() {
 
     # Download and install Terraform v1.1.7 as that is the version used for the development of this code-base.
     # TODO: Once Base and Ceiling versions have been validated, the code here will be modified to download the Ceiling version of terraform as required by the scripts in this code-base.
-    mkdir ${HOME}/bin/ && cd ${HOME}/bin/ && wget $TERRAFORM_DOWNLOAD_URL && unzip $TERRAFORM_DOWNLOAD_FILE && rm $TERRAFORM_DOWNLOAD_FILE
+    mkdir -p ${HOME}/bin/ && cd ${HOME}/bin/ && wget $TERRAFORM_DOWNLOAD_URL && unzip $TERRAFORM_DOWNLOAD_FILE && rm $TERRAFORM_DOWNLOAD_FILE
 
     # Display an confirmation of the successful installation of Terraform.
     echo "Installed: `${HOME}/bin/terraform version`"
 }
 
 function install_kubectl() {
+    # We will use Kubectl v1.23.0 because we faced some issues with using the latest version (v1.24.0).
     KUBECTL_VERSION="1.23.0"
+
+    # Check if kubectl is already installed and display the version as installed.
+    [[ -f ${HOME}/bin/kubectl ]] && echo "kubectl is already installed at ${HOME}/bin/kubectl" && return 0
+
     KUBECTL_DOWNLOAD_URL="https://dl.k8s.io/release/v$KUBECTL_VERSION/bin/linux/amd64/kubectl"
-    cd ${HOME}/bin/ && curl -LO $KUBECTL_DOWNLOAD_URL && chmod +x kubectl
+
+    echo "Downloading Kubectl v$KUBECTL_VERSION from '$KUBECTL_DOWNLOAD_URL'"
+
+    # Download and install kubectl v1.23.0, move it to the bin folder
+    mkdir -p ${HOME}/bin/ && cd ${HOME}/bin/ && curl -LO $KUBECTL_DOWNLOAD_URL && chmod +x kubectl
 }
 
 function install_aws_iam_authenticator() {
+    # Using whatever was the latest stable version at the time of development.
     AWS_IAM_AUTH_VERSION="1.21.2"
+
+    # Check if aws-iam-authenticator is already installed and display the version as installed.
+    [[ -f ${HOME}/bin/aws-iam-authenticator ]] && echo "aws-iam-authenticator is already installed at ${HOME}/bin/aws-iam-authenticator" && return 0
+
     AWS_IAM_AUTH_DOWNLOAD_URL="https://s3.us-west-2.amazonaws.com/amazon-eks/$AWS_IAM_AUTH_VERSION/2021-07-05/bin/linux/amd64/aws-iam-authenticator"
-    cd ${HOME}/bin/ && curl -o aws-iam-authenticator $AWS_IAM_AUTH_DOWNLOAD_URL && chmod +x aws-iam-authenticator
+
+    # Download and install aws-iam-authenticator v1.21.2, move it to the bin folder
+    mkdir -p ${HOME}/bin/ && cd ${HOME}/bin/ && curl -o aws-iam-authenticator $AWS_IAM_AUTH_DOWNLOAD_URL && chmod +x aws-iam-authenticator
 }
 
 function deploy_vmseries_lab() {
@@ -46,13 +62,13 @@ function deploy_vmseries_lab() {
     terraform init
 
     # Deploy resources
-    echo "Deploying Resources required for Palo Alto Networks Reference Architecture for Zero Trust on AWS"
+    echo "Deploying Resources required for Palo Alto Networks Reference Architecture for Zero Trust with VM-Series on AWS"
     terraform apply -auto-approve
 
     if [ $? -eq 0 ]; then
-        echo "AWS Zero Trust Reference Architecture Lab Deployment Completed successfully!"
+        echo "AWS Zero Trust Reference Architecture with VM-Series Lab Deployment Completed successfully!"
     else
-        echo "AWS Zero Trust Reference Architecture Lab Deployment Failed!"
+        echo "AWS Zero Trust Reference Architecture with VM-Series Lab Deployment Failed!"
     fi
 }
 
@@ -65,13 +81,14 @@ function deploy_cnseries_lab() {
     terraform init
 
     # Deploy resources
-    echo "Deploying Resources required for Palo Alto Networks Reference Architecture for Zero Trust on AWS"
+    echo "Deploying Resources required for Palo Alto Networks Reference Architecture for Zero Trust with CN-Series on AWS"
     terraform apply -auto-approve
 
     if [ $? -eq 0 ]; then
-        echo "AWS Zero Trust Reference Architecture Lab Deployment Completed successfully!"
+        echo "AWS Zero Trust Reference Architecture with CN-Series Lab Deployment Completed successfully!"
     else
-        echo "AWS Zero Trust Reference Architecture Lab Deployment Failed!"
+        echo "AWS Zero Trust Reference Architecture with CN-Series Lab Deployment Failed!"
+        echo "Please try updating the kubeconfig and run setup again. Check the Lab Guide for more details."
     fi
 }
 
@@ -80,4 +97,18 @@ install_terraform
 install_kubectl
 install_aws_iam_authenticator
 
-deploy_vmseries_lab
+if [ $# -gt 0 ]
+then
+    if [ $1 == "vm" ]
+    then
+        deploy_vmseries_lab
+    elif [ $1 == "cn" ]
+    then
+        deploy_cnseries_lab
+    else
+        echo "Bad argument!"
+    fi
+else
+    # Deploy VM-Series lab if no argument is provided.
+    deploy_vmseries_lab
+fi
